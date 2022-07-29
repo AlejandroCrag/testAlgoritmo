b719894f36614f4e14fe5046b5bd462852b7af03
Alex <alejandro_cruz@biometria.com.mx>
2022-07-29 13:16:41 -0500

    Algoritmo Remaster

--- AlgoritmoTest/AlgoritmoTest/ProcesoComparacion.cs
+++ AlgoritmoTest/AlgoritmoTest/ProcesoComparacion.cs
@@ -8,28 +8,26 @@ namespace AlgoritmoTest
 {
     class ProcesoComparacion
     { 
-        public bool CrearSubClusters { get; internal set; }
-
-        public bool Init = false;
+        //sabremos en que parte del proceso se encuentra 
+        //0 => Sin Iniciar -> se busca archivo a cargar
+        //1 => Proceso AB FaltantesVsSubClusters
+        //2 => Proceso BB FaltantesVsFaltantes
+        //3 => Proceso Finalizado Escribir Resultados
+        //4 => Buscar nuevo archivo a cargar
+        public int StatusProceso = 0;
 
         public List<DatosRegistro> ElementosFaltantesList { get; set; }
         public List<SubCluster> SubClusterActuales { get; set; }
-
         public int TotalElementosFaltantes { get; set; }
-        public int Clouster_Id { get; private set; }
-
-        public int SubClusterIdActual;
-
-        public int ElementoActual = 0;
+        public int Clouster_Id { get; private set; } 
 
         private Configuracion datosConfiguracion;
-
         public bool Start()
         {
-            CrearSubClusters = true;
+            GetSubClustersActuales();
             for (int a = 0; a < TotalElementosFaltantes; a++)
             {
-                ElementoActual = a;
+                //saveConfiguracion("", "", "");
                 bool resultado = ElemFaltantesvsSubClusters(ElementosFaltantesList[a]);
 
                 if (resultado)
@@ -38,25 +36,18 @@ namespace AlgoritmoTest
                     return true;
                 }
             }
-            //saveConfiguracion("", "", "");
+            //Status Incrementa en 1=> 
+            //Siguiente Paso es comparar FaltantesVsFaltantes
+            //O Escribir los Resultados
+            StatusProceso++;
             return false;
         }
 
-        private bool ElemFaltantesvsSubClusters(DatosRegistro Cu)
+        private bool ElemFaltantesvsSubClusters(DatosRegistro ClienteUnico)
         {
-            GetSubClustersActuales();
-
-            SubClusterIdActual = 1;
-            ConvertElementosFaltantesToSubClusters();
-
             foreach (var SCI in SubClusterActuales)
-            {
-                if (SubClusterIdActual != 0)
-                {
-                    SubClusterIdActual = SCI.Id;
-                }
-
-                var resultadoPermutacion = ProcesoPermutacion(Cu, SCI, SubClusterIdActual);
+            { 
+                var resultadoPermutacion = ProcesoPermutacion(ClienteUnico, SCI);
                 if (resultadoPermutacion)
                 {
                     return true;
@@ -74,7 +65,6 @@ namespace AlgoritmoTest
             ElementosFaltantesList.Add(new DatosRegistro(4, "x", "y"));
             ElementosFaltantesList.Add(new DatosRegistro(5, "x", "y"));
             ElementosFaltantesList.Add(new DatosRegistro(6, "x", "y"));
-            
             TotalElementosFaltantes = ElementosFaltantesList.Count();
         }
 
@@ -88,28 +78,44 @@ namespace AlgoritmoTest
             }
         }
 
-        private void AgregarAlSubCluster(DatosRegistro Cu, int SubClusId)
+        private void AgregarAlSubCluster(DatosRegistro clienteAgregar, int IdSubCluster)
         {
-            if (CrearSubClusters)
-            {
-                var data = new SubCluster(SubClusterActuales.Count + 1, Clouster_Id, new List<DatosRegistro>());
-                data.GrupoRegistros.Add(Cu);
+            /*Comprobar si el elemento a agregar no existe dentro del arreglo*/
+            var obtenerSub = SubClusterActuales.Find(x => x.Id == IdSubCluster);
+            var cliente = obtenerSub.GrupoRegistros.Count(y=> y.Id == clienteAgregar.Id);
+            var agregado = false;
+            if (cliente==0) {
+                //Posiblemente en el proceso de FaltanteVsFaltante los SubCluster No existan
+
+                if (StatusProceso == 2)
+                {
+                    //Revisar si el clouster existe
+                    // getIdIfExist(SubClusterActuales);
+                }
+                //AgregarAlSubCluster
+                //var data = new SubCluster(SubClusterActuales.Count + 1, Clouster_Id, new List<DatosRegistro>());
+                //data.GrupoRegistros.Add(clienteAgregar); 
+                if (agregado) {
+                    RemoverDeFaltantes(clienteAgregar);
+                }
             }
         }
         
-        private bool ProcesoPermutacion(DatosRegistro cu, SubCluster sCI, int subClusterIdActual)
+        private bool ProcesoPermutacion(DatosRegistro Cliente, SubCluster subClusterActual)
         {
-            var elementosEnSubCluster = sCI.GrupoRegistros.Count();
+            var elementosEnSubCluster = subClusterActual.GrupoRegistros.Count();
             var newSubClus = new List<DatosRegistro>();
             int coincidencias = 0;
-            foreach (var CuSub in sCI.GrupoRegistros)
+            foreach (var ClienteSub in subClusterActual.GrupoRegistros)
             {
-                if (CuSub.Id != cu.Id)
+                if (ClienteSub.Id != Cliente.Id)
                 {
-                    if (Compara(CuSub, cu) || Compara(CuSub, cu))
+                    if (Compara(ClienteSub, Cliente) || Compara(ClienteSub, Cliente))
                     {
-                        newSubClus.Add(cu);
-                        newSubClus.Add(CuSub);
+                        newSubClus.Add(Cliente);
+                        if (StatusProceso == 2) {
+                            newSubClus.Add(ClienteSub);
+                        }
                         coincidencias++;
                     }
                 }
@@ -118,18 +124,10 @@ namespace AlgoritmoTest
                 }
             }
 
-            //if (coincidencias > (float)(elementosEnSubCluster / 2)) {
-            if (CrearSubClusters)
+            if (coincidencias > (float)(elementosEnSubCluster / 2)) {
+                foreach (var newElement in newSubClus)
                 {
-                    if (CrearSubClusters) {
-                    foreach (var newElement in newSubClus) {
-                        AgregarAlSubCluster(newElement, subClusterIdActual);
-                        RemoverDeFaltantes(newElement);
-                    }
-                }
-                else {
-                    AgregarAlSubCluster(cu, subClusterIdActual);
-                    RemoverDeFaltantes(cu);
+                    AgregarAlSubCluster(newElement, subClusterActual.Clouster_Id);
                 }
                 return true;
             }
@@ -143,16 +141,18 @@ namespace AlgoritmoTest
 
         private void GetSubClustersActuales()
         {
-            new ConsultaSubClusters().readJsonData();
-            if (!CrearSubClusters)
-            {
+            //Obtenemos los SubClusters Recibidos de la comparacion
+            if (this.StatusProceso == 1) {
                 SubClusterActuales = new ConsultaSubClusters().ListadoSubClusters();
-
-                if (SubClusterActuales.Count() == 0) {
-                    ConvertElementosFaltantesToSubClusters();
+                //En Caso de no contar con ningun SubCluster tendremos que avanzar al siguiente status del proceso
+                if (SubClusterActuales.Count() == 0)
+                {
+                    this.StatusProceso = 2;
                 }
-            } else if (CrearSubClusters)
-            {
+            }
+            //Si se finalizo la comparacion O No Existen elemento con que comparar
+            //Creamos Elementos a Comparar apartir de los mismo elementos faltantes
+            if (this.StatusProceso == 2) {
                 ConvertElementosFaltantesToSubClusters();
             }
         }
@@ -167,8 +167,7 @@ namespace AlgoritmoTest
                 data.GrupoRegistros.Add(CU);
                 SubClusterActuales.Add(data);
                 id++;
-            }
-            CrearSubClusters = true;
+            } 
         }
 
         private void SaveConfiguracion(String clouster,String subCluster,String DatosRegistro)
@@ -182,6 +181,10 @@ namespace AlgoritmoTest
             datosConfiguracion.Ultimo_clouster = 1;
             datosConfiguracion.Ultimo_sub_clouster = 1;
             datosConfiguracion.UltimoRegistro = new DatosRegistro(1,"","");
+            if (this.StatusProceso==0) {
+                new ConsultaSubClusters().readJsonData();
+                this.StatusProceso = 1;
+            }
         }
     }
 }
--- AlgoritmoTest/AlgoritmoTest/Program.cs
+++ AlgoritmoTest/AlgoritmoTest/Program.cs
@@ -9,33 +9,29 @@ namespace AlgoritmoTest
         static void Main(string[] args)
         {
             proceso = new ProcesoComparacion();
-            Init();
-            Console.WriteLine("end");
+            proceso.LoadConfiguracion();
+            Iniciar(); 
         }
 
-        private static void Init()
-        {
-            if (!proceso.Init) {
-                proceso.Init = true;
-            }
-
+        private static void Iniciar()
+        { 
             proceso.GetElementosFaltantes();
-            proceso.LoadConfiguracion();
-
+             
             var resultado = proceso.Start();
-            try
-            {
-                Console.WriteLine(proceso.ElementoActual);
-            }
-            catch (StackOverflowException e) {
-                Console.WriteLine(e.Message);
-            }
-            
+
             if (resultado)
             {
-                Init();
-            } else if (proceso.ElementoActual < proceso.TotalElementosFaltantes-1 ) {
-                Init();
+                Iniciar();
+            }
+            else if (proceso.StatusProceso < 3)
+            {
+                Iniciar();
+            }
+            else if (proceso.StatusProceso == 3) {
+                //Escribir Resultados
+            }
+            else if (proceso.StatusProceso == 4) { 
+                //Cargar Nuevo Documento
             }
         }
     }
